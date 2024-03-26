e8182fc10ad4ae523bd16a0b40486d8c8e1621ec
Kenneth Jay Bruno <kjdbruno@gmail.com>
2024-03-26 11:56:01 +0800

    login
    set guide

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -4,24 +4,20 @@
             <div :class="$q.dark.isActive ? 'wrapper-container-dark' : 'wrapper-container-light'">
                 <div class="wrapper-login">
                     <div class="wrapper-pic js-tilt" data-tilt>
-                        <img class="logo" src="~assets/logo.png" alt="IMG">
+                        <!-- <img class="logo" src="~assets/logo.png" alt="IMG"> -->
+                        logo here
                     </div>
                     <div class="wrapper-form">
 
                         <!--input username-->
-                        <q-input outlined v-model="username" label="Username" :color="$q.dark.isActive ? 'white' : 'primary'" class="full-width q-mb-xs" :error="errors.username.type" :error-message="errors.username.msg" autofocus>
+                        <q-input outlined v-model="username" label="Username" color="dark" class="full-width q-mb-xs" autofocus>
                             <template v-slot:prepend>
                                 <q-icon name="account_circle" style="font-size: 1rem;" />
                             </template>
                         </q-input>
-                        <!--error: username-->
-                        <div class="q-mb-xs q-pa-xs bg-red full-width radius-xs text-white" v-if="errors.username.type">
-                            <q-icon name="error" size="1.2em" color="white"/>
-                            <span class="text-caption q-ml-sm">{{ errors.username.msg }}</span>
-                        </div>
 
                         <!--input password-->
-                        <q-input outlined v-model="password" :type="isPwd ? 'password' : 'text'" label="Password" :color="$q.dark.isActive ? 'white' : 'primary'" class="full-width q-mb-xs" :error="errors.password.type" :error-message="errors.password.msg">
+                        <q-input outlined v-model="password" :type="isPwd ? 'password' : 'text'" label="Password" color="dark" class="full-width q-mb-xs">
                             <template v-slot:prepend>
                                 <q-icon name="lock" style="font-size: 1rem;" />
                             </template>
@@ -29,19 +25,10 @@
                                 <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" style="font-size: 1.2rem;" />
                             </template>
                         </q-input>
-                        <!--error: password-->
-                        <div class="q-mb-xs q-pa-xs bg-red full-width radius-xs text-white" v-if="errors.password.type">
-                            <q-icon name="error" size="1.2em" color="white"/>
-                            <span class="text-caption q-ml-sm">{{ errors.password.msg }}</span>
-                        </div>
 
                         <!--button-->
                         <div class="q-mt-sm full-width">
-                            <q-btn v-ripple unelevated color="primary" size="lg" label="sign in" text-color="white" class="text-capitalize full-width" :loading="loading" @click="login">
-                                <template v-slot:loading>
-                                <q-spinner-puff color="white" />&nbsp;Signing in...
-                                </template>
-                            </q-btn>
+                            <q-btn v-ripple unelevated color="dark" size="lg" label="sign in" class="full-width" @click="login"/>
                         </div>
                     </div>
                 </div>
@@ -70,50 +57,6 @@ const password = ref('')
  */
 const isPwd = ref(true)
 /**
- * declare loading
- */
-const loading = ref(false)
-/**
- * declare error
- */
-const errors = reactive({
-    username: { msg: null, type: null },
-    password: { msg: null, type: null },
-})
-/**
- * validate input
- */
- const validation = () => {
-    let isError = false
-    //username
-    if (username.value.length < 1) {
-        //
-        errors.username.msg = 'Please enter username'
-        errors.username.type = true
-        isError = true
-    } else {
-        errors.username.msg = null
-        errors.username.type = null
-    }
-    //password
-    if (password.value.length < 1) {
-        //
-        errors.password.msg = 'Please enter your password'
-        errors.password.type = true
-        isError = true
-    } else if (password.value.length > 0 && password.value.length < 4) {
-        //
-        errors.password.msg = 'Minimum password length is 4'
-        errors.password.type = true
-        isError = true
-    } else {
-        errors.password.msg = null
-        errors.password.type = null
-    }
-
-    return isError
-}
-/**
  * login function
  */
 const login = async () => {
@@ -125,19 +68,42 @@ const login = async () => {
     if (isError) return false
 
     /**
-     * set loading to TRUE
+     * perform loading
      */
-    loading.value = true
 
     /**
-     * perform try catch
+     * perform try catch function
      */
     try {
 
+        /**
+         * send request to backend
+         * -> use POST method
+         */
+
+        /**
+         * retrieve response
+         */
+
+        /**
+         * set token
+         */
+
+        /**
+         * set authenticated user
+         */
+
+        /**
+         * navigate to home route
+         */
+
     } catch (error) {
 
-    }
+        /**
+         * catch error
+         */
 
+    }
 }
 </script>
 
